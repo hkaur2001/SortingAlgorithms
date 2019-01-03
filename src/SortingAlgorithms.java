d6ef39b6ed1ca1f3c3036d3d02f40ee67e4f9fc9
BT_1N3_28@BKTECH.LOCAL <BT_1N3_28@BKTECH.LOCAL>
2019-01-03 14:46:12 -0500

    Initial commit

---
+++ src/SortingAlgorithms.java
@@ -0,0 +1,19 @@
+public class SortingAlgorithms {
+    public static void swap(int[]arr,int i, int j)
+    {
+       int temp = arr[i];
+       arr[j] = arr[i];
+       arr[j] = temp;
+    }
+
+    public static void bubbleSort(int [] arr)
+    {
+        int swapcount;
+        swapcount = 0;
+        while (swapcount>0){
+            for(int i= 0; i< arr.length; i ++){
+                if (arr)
+            }
+        }
+    }
+}
